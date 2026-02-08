2557d86137ac49eabbeb81702225b54aec5fa3bc
Mohi Hassan <mohi.hassan@mail.com>
2026-02-08 05:04:51 +0200

    feat: Add initial Android and iOS app assets, including icons and splash screens, and configure core Android project files.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.mybrain.app',
   appName: 'Brainia',
-  webDir: 'out',
-  server: {
-    url: 'http://192.168.1.237:3000',
-    cleartext: true
-  }
+  webDir: 'out'
 };
 
 export default config;
